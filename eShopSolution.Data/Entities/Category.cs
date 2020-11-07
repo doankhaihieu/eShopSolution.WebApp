d7d1e979b91a80bbda24ea7491804c080af8d409
doan khai hieu <doankhaihieu@gmail.com>
2020-11-07 23:37:52 +0700

    23:38 07/11/2020

--- eShopSolution.Data/Entities/Category.cs
+++ eShopSolution.Data/Entities/Category.cs
@@ -10,8 +10,12 @@ namespace eShopSolution.Data.Entities
         public int Id { set; get; }
         public int SortOrder { set; get; }
         public bool IsShowOnHome { set; get; }
-        public int ParentId { set; get; }
+        public int? ParentId { set; get; }
         public Status Status { set; get; }
+
+        public List<ProductInCategory> ProductInCategories { get; set; }
+
+        public List<CategoryTranslation> CategoryTranslations { get; set; }
 
     }
 }
